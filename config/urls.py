e8ada53e4ff2c8ed2aa782f9b4e633c11582d59b
seonseon933 <seonseon933@gmail.com>
2024-03-08 18:33:04 +0900

    오류 페이지 생성

--- config/urls.py
+++ config/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'), # settings의 '/'에 해당됨.
 ]
+handler404 = 'common.views.page_not_found'
+# 오류 발생 시 사용자가 정의한 뷰 함수가 호출됨. 
+# handler404 변수는 반드시 config/urls.py에 등록해야 함.
